7e1cfcf4d9960025fd93680f15c8e199f4795c9e
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-07-21 15:03:48 +0300

    remove Gifts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import dynamic from "next/dynamic";
 import ScrollToTopButton from "@/components/ui/ScrollToTop/ScrollToTopButton";
 const HeroProducts = dynamic(
-    () => import("@/components/Products/HeroProduct/HeroProducts")
+  () => import("@/components/Products/HeroProduct/HeroProducts")
 );
 const RecommendedCategoriesComponent = dynamic(
-    () => import("@/components/Category/RecommendedCategories")
+  () => import("@/components/Category/RecommendedCategories")
 );
-const Gifts = dynamic(() => import("@/components/Gifts/Gifts"));
+// const Gifts = dynamic(() => import("@/components/Gifts/Gifts"));
 const RecommendedProducts = dynamic(
-    () => import("@/components/Products/RecommendedProduct/RecommendedProducts")
+  () => import("@/components/Products/RecommendedProduct/RecommendedProducts")
 );
 export default function Home() {
-    return (
-        <div className="space-y-8 contain my-6">
-            <HeroProducts />
-            <RecommendedCategoriesComponent />
-            <Gifts />
-            <RecommendedProducts />
-            <ScrollToTopButton />
-        </div>
-    );
+  return (
+    <div className="space-y-8 contain my-6">
+      <HeroProducts />
+      <RecommendedCategoriesComponent />
+      {/* <Gifts /> */}
+      <RecommendedProducts />
+      <ScrollToTopButton />
+    </div>
+  );
 }
